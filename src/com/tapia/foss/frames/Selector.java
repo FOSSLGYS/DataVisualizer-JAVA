9224c0a1a112ff2dcc83ccd7163e48b12fe31243
Foss Tester <testerfoss@gmail.com>
2016-09-14 21:54:10 -0500

    Update directory structure

--- src/com/tapia/foss/frames/Selector.java
+++ src/com/tapia/foss/frames/Selector.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package com.ghc.foss.frames;
+package com.tapia.foss.frames;
 
 import com.tapia.foss.ds.List;
 import com.tapia.foss.ds.BinaryTree;
